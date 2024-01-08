557757bcc1d3f2a06766310eec0defa0bdb67514
Falowo Thompson <57360209+thompsongboyega2019@users.noreply.github.com>
2024-01-09 00:01:07 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,25 @@
 function selectSignUp() {
     var a = document.getElementById("registerationForm");
     a.classList.add("d-none");
+    var residents = document.getElementById("residents");
+    var estateManager = document.getElementById("estateManager");
+    var excos = document.getElementById("excos");
+    var artisanSecurity = document.getElementById("artisanSecurity");
+
+    residents.addEventListener("click", function() {
+         a.classList.remove("d-none");
+         let showResidents = document.getElementsByClassName("landlordOrTenant");
+         showResidents[0].classList.remove("d-none");
+         let hideArtisanSecurity = document.getElementsByClassName("artisanOrSecurity");
+         hideArtisanSecurity[0].classList.add("d-none");
+    });
+    artisanSecurity.addEventListener("click", function() {
+        a.classList.remove("d-none"); 
+        let showArtisanSecurity = document.getElementsByClassName("artisanOrSecurity");
+        showArtisanSecurity[0].classList.remove("d-none");
+        let hideResidents = document.getElementsByClassName("landlordOrTenant");
+        hideResidents[0].classList.add("d-none");
+    })
+    
 }
 selectSignUp();
