d4fc783803427b2aa4a2044af378dbed778416a2
Falowo Thompson <57360209+thompsongboyega2019@users.noreply.github.com>
2024-01-16 14:36:30 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -4,49 +4,81 @@ function selectSignUp() {
     var a = document.getElementById("registerationForm");
     a.classList.add("d-none");
     var residents = document.getElementById("residents");
-    var subResident = document.getElementById("sub-resident");
     var estateManager = document.getElementById("estateManager");
     var excos = document.getElementById("excos");
     var artisanSecurity = document.getElementById("artisanSecurity");
+    var hohField = document.getElementById("hoh-id-field");
+    hohField.classList.add("d-none");
     var scrollText = document.getElementById("scrollText");
     scrollText.classList.add("d-none");
+    let generalFields = document.getElementsByClassName("general-fields");
 
     residents.addEventListener("click", function () {
+        residents.getElementsByClassName("bg-white")[0].classList.add("category-active", "shadow-lg");
         a.classList.remove("d-none");
         let showResidents = document.getElementsByClassName("landlordOrTenant");
         showResidents[0].classList.remove("d-none");
-        let hideHohId = document.getElementById("hoh-id-field");
+        showResidents[1].classList.add("d-none");
+        for (const x of generalFields) {
+            x.classList.add("d-none");
+        }
+        artisanSecurity.getElementsByClassName("bg-white")[0].classList.remove("category-active", "shadow-lg");
         let hideArtisanSecurity = document.getElementsByClassName("artisanOrSecurity");
-        hideHohId.classList.add("d-none");
         hideArtisanSecurity[0].classList.add("d-none");
         scrollText.classList.remove("d-none");
     });
-    subResident.addEventListener("click", function () {
-        a.classList.remove("d-none");
-        let showHohId = document.getElementById("hoh-id-field");
-        showHohId.classList.remove("d-none");
-        let hideResidents = document.getElementsByClassName("landlordOrTenant");
-        let hideArtisanSecurity = document.getElementsByClassName("artisanOrSecurity");
-        hideResidents[0].classList.add("d-none");
-        hideArtisanSecurity[0].classList.add("d-none");
-        scrollText.classList.remove("d-none");
-
-
-    })
     artisanSecurity.addEventListener("click", function () {
+        artisanSecurity.getElementsByClassName("bg-white")[0].classList.add("category-active", "shadow-lg");
         a.classList.remove("d-none");
         let showArtisanSecurity = document.getElementsByClassName("artisanOrSecurity");
         showArtisanSecurity[0].classList.remove("d-none");
         let hideResidents = document.getElementsByClassName("landlordOrTenant");
-        let hideHohId = document.getElementById("hoh-id-field");
-        hideHohId.classList.add("d-none");
-        hideResidents[0].classList.add("d-none");
+        for (const x of hideResidents) {
+            x.classList.add("d-none");
+        }
+        for (const x of generalFields) {
+            x.classList.remove("d-none");
+        }
+        residents.getElementsByClassName("bg-white")[0].classList.remove("category-active", "shadow-lg");
         scrollText.classList.remove("d-none");
     })
 
 }
 selectSignUp();
 // Sign up category rgistration field ends
+
+
+// Account type option controller starts 
+function chooseAccountType() {
+    let generalFields = document.getElementsByClassName("general-fields");
+    let showResidents = document.getElementsByClassName("landlordOrTenant");
+    var hohField = document.getElementById("hoh-id-field");
+    let a = document.getElementById("account-type");
+    switch (a.selectedIndex) {
+        case 1:
+            for (const x of generalFields) {
+                x.classList.remove("d-none");
+            };
+            hohField.classList.add("d-none");
+            showResidents[1].classList.remove("d-none");
+            break;
+        case 2:
+            for (const x of generalFields) {
+                x.classList.remove("d-none");
+            };
+            hohField.classList.remove("d-none");
+            showResidents[1].classList.add("d-none");
+            break;
+        case 0:
+            for (const x of generalFields) {
+                x.classList.add("d-none");
+            };
+            hohField.classList.add("d-none");
+            showResidents[1].classList.add("d-none");
+            break;
+    };
+}
+// Account type option controller ends 
 
 // Head Of House info controller & HOH Id validation start
 function displayHohInfo() {
@@ -59,7 +91,7 @@ var verifyHohId = document.querySelector("#hohId + input");
 verifyHohId.addEventListener("blur", showInfo);
 
 function showInfo() {
-    if (verifyHohId.value == "") {
+    if (verifyHohId.value === "") {
         document.querySelector("#id-error-message").innerHTML = "<b><i>Please provide your Head Of House Id</i></b>";
         document.querySelector("#hoh-info").classList.add("d-none");
     } else {
@@ -68,3 +100,4 @@ function showInfo() {
     }
 }
 // Head Of House info controller & HOH Id validation end
+
